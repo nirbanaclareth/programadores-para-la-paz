73dec25a693b055a85a38789c0e9466824d8dd3e
elianaarnedo <arnedomoraleseliana@gmail.com>
2026-07-14 21:45:00 -0500

    actividad realizada

---
+++ semana5/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// Ruta principal
+app.get('/', (req, res) => {
+  res.send('Servidor activo');
+});
+
+// Ruta de saludo
+app.get('/saludo', (req, res) => {
+  res.send('Hola comunidad');
+});
+
+// Ruta con parámetro
+app.get('/mensaje/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send('Hola ' + nombre);
+});
+
+// === PREGUNTA 8: Ruta POST ===
+app.post('/reporte', (req, res) => {
+  const mensaje = req.body.mensaje;
+  
+  res.json({
+    estado: "Reporte recibido",
+    mensaje: mensaje
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Servidor ejecutándose en puerto 3000');
+});
